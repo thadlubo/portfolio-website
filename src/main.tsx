cc4137edc8e9f97e65e0f6a02b569d8c4173ba49
Thaddeus <thadlubo@gmail.com>
2025-10-02 16:39:22 +0100

    vite and json config for github io

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio-website">
       <App />
     </BrowserRouter>
   </React.StrictMode>
